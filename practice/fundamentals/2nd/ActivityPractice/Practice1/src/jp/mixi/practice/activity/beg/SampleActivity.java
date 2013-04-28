def69a3fe3ac099c3156d3be877d87a2ddbb6e14
Jun HASHIMOTO <fusui.dreamer@gmail.com>
2013-04-28 15:30:29 +0900

    solve ActivityPractice1

---
+++ practice/fundamentals/2nd/ActivityPractice/Practice1/src/jp/mixi/practice/activity/beg/SampleActivity.java
@@ -0,0 +1,11 @@
+package jp.mixi.practice.activity.beg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity{
+	public void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sample);
+	}
+}
