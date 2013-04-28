c950b5a5246e2bb9010d9e11790c761d412be8c6
Jun HASHIMOTO <fusui.dreamer@gmail.com>
2013-04-28 23:51:40 +0900

    solve ControllerAssignment3

--- assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MainActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MainActivity.java
@@ -3,14 +3,19 @@ package jp.mixi.assignment.controller.adv;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		FragmentManager manager = getSupportFragmentManager();
+		Fragment fragment = manager.findFragmentById(R.id.MyFragment);
     }
 
     @Override
