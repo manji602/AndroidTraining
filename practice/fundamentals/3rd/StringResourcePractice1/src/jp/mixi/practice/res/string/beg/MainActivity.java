55f1c7e369a377158ca74a96ab999911b14ec047
橋本 淳 <jun.hashimoto@mixi.co.jp>
2013-05-07 20:12:34 +0900

    solve StringResourcePractice1

--- practice/fundamentals/3rd/StringResourcePractice1/src/jp/mixi/practice/res/string/beg/MainActivity.java
+++ practice/fundamentals/3rd/StringResourcePractice1/src/jp/mixi/practice/res/string/beg/MainActivity.java
@@ -1,16 +1,40 @@
 
 package jp.mixi.practice.res.string.beg;
 
+import java.util.Locale;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        //locale ENGLISH
+        Locale locale = Locale.ENGLISH;
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        Resources resources = getBaseContext().getResources();
+        resources.updateConfiguration(config, null);
         setContentView(R.layout.activity_main);
+        
+        //monday
+        String mondays = getResources().getQuantityString(R.plurals.monday, 2);
+        TextView textviewMonday = (TextView)findViewById(R.id.monday);
+        textviewMonday.setText(mondays);
+        
+        //tuesday
+        String formattedTuesday = getString(R.string.tuesday, 5, 7);        
+        TextView textviewTuesday = (TextView)findViewById(R.id.tuesday);
+        textviewTuesday.setText(formattedTuesday);
+        
     }
 
     @Override
