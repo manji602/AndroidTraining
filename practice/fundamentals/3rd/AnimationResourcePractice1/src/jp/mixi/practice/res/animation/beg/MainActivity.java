331350973577f5f5511ca9981c72f0d46c947161
Jun HASHIMOTO <fusui.dreamer@gmail.com>
2013-05-11 23:35:49 +0900

    solve AnimationResourcePractice1

--- practice/fundamentals/3rd/AnimationResourcePractice1/src/jp/mixi/practice/res/animation/beg/MainActivity.java
+++ practice/fundamentals/3rd/AnimationResourcePractice1/src/jp/mixi/practice/res/animation/beg/MainActivity.java
@@ -4,6 +4,10 @@ package jp.mixi.practice.res.animation.beg;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +15,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final View text = findViewById(R.id.hello_world);
+        Button button = (Button)findViewById(R.id.button_start_animation);
+        
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.sample_animation);
+                text.startAnimation(animation);                
+            }
+            
+        });
     }
 
     @Override
