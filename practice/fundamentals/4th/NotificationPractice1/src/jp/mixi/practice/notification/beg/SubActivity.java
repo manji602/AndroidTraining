f2edd1664de999f8ffe8d72e351034927e5f7434
Jun HASHIMOTO <fusui.dreamer@gmail.com>
2013-05-12 19:35:01 +0900

    solve NotificationPractice1

---
+++ practice/fundamentals/4th/NotificationPractice1/src/jp/mixi/practice/notification/beg/SubActivity.java
@@ -0,0 +1,19 @@
+
+package jp.mixi.practice.notification.beg;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+public class SubActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+               
+    }
+}
