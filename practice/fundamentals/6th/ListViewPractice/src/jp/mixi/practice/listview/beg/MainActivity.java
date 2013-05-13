f4e2171dc43572244650603ec89d950e5b201fdf
橋本 淳 <jun.hashimoto@mixi.co.jp>
2013-05-13 14:48:45 +0900

    solve ListViewPractice

--- practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
+++ practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
@@ -1,16 +1,55 @@
 
 package jp.mixi.practice.listview.beg;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
 
+	private MainActivity mActivity;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+mActivity = this;
+        
+        // ListViewに表示するデータを作成する
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            list.add("hoge" + i);
+        }
+
+        final ListView listView = (ListView) findViewById(R.id.ListView);
+        CustomListItemAdapter adapter = new CustomListItemAdapter(mActivity, list);
+
+        listView.setAdapter(adapter);
+        // リストアイテムをタップした時の動作を定義する
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Adapterからタップした位置のデータを取得する
+                String str = (String) parent.getItemAtPosition(position);
+                Toast.makeText(mActivity, str, Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        //Button listener
+        Button button = (Button)findViewById(R.id.ScrollButton);
+        button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				listView.smoothScrollToPosition(0);
+			}
+		});
     }
 
     @Override
