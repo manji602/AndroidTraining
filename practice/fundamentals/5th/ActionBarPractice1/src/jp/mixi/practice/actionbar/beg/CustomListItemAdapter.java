9063c4cd00c250f843429288003d5c62f69b899f
橋本 淳 <jun.hashimoto@mixi.co.jp>
2013-05-13 13:00:35 +0900

    solve ActionBarPractice1

---
+++ practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/CustomListItemAdapter.java
@@ -0,0 +1,50 @@
+package jp.mixi.practice.actionbar.beg;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class CustomListItemAdapter extends ArrayAdapter<String> {
+
+    private LayoutInflater mLayoutInflater;
+
+    public CustomListItemAdapter(Context context, List<String> objects) {
+        // 第2引数はtextViewResourceIdとされていますが、カスタムリストアイテムを使用する場合は特に意識する必要のない引数です
+        super(context, 0, objects);
+        // レイアウト生成に使用するインフレーター
+        mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = null;
+
+        // ListViewに表示する分のレイアウトが生成されていない場合レイアウトを作成する
+        if (convertView == null) {
+            // レイアウトファイルからViewを生成する
+            view = mLayoutInflater.inflate(R.layout.custom_list_item, parent, false);
+            
+            // リストアイテムに対応するデータを取得する
+            String item = getItem(position);
+
+            // 各Viewに表示する情報を設定
+            TextView text1 = (TextView) view.findViewById(R.id.TitleText);
+            text1.setText("Title:" + item);
+            TextView text2 = (TextView) view.findViewById(R.id.SubTitleText);
+            text2.setText("SubTitle:" + item);
+
+        } else {
+            // レイアウトが存在する場合は再利用する
+            view = convertView;
+        }
+        // 一度作成したレイアウトの表示データを変更しないことにより、再利用されたデータがどこに表示されるかを確認する
+        return view;
+    }
+
+}
