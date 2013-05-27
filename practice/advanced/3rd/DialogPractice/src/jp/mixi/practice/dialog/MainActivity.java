77229d63abba59cb38c8614889206749c4ae0de4
橋本 淳 <jun.hashimoto@mixi.co.jp>
2013-05-27 12:53:43 +0900

    solve DialogPractice

--- practice/advanced/3rd/DialogPractice/src/jp/mixi/practice/dialog/MainActivity.java
+++ practice/advanced/3rd/DialogPractice/src/jp/mixi/practice/dialog/MainActivity.java
@@ -1,12 +1,16 @@
 
 package jp.mixi.practice.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -25,6 +29,9 @@ public class MainActivity extends FragmentActivity {
 
     private void showPracticeDialog() {
         // TODO:ダイアログを表示する処理を実装してください
+    	DialogFragment myDialogFragment = new PracticeDialogFragment();
+        // 引数にFramentManagerとtagを設定します
+        myDialogFragment.show(getSupportFragmentManager(), "my_dialog_fragment");
     }
 
     @Override
@@ -36,6 +43,21 @@ public class MainActivity extends FragmentActivity {
 
     // TODO:独自DialogFragmentを実装してください
     public static class PracticeDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+        	// AlertDialogはBuilderパターンで生成
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+            builder.setIcon(R.drawable.ic_launcher)
+            .setTitle(R.string.practice)
+            .setMessage(R.string.message)
+            .setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Toast.makeText(getActivity(), "closed", Toast.LENGTH_SHORT).show();
+                }
+            });
+            // Dialogを作成して返却
+            return builder.create();
+        }
     }
 }
