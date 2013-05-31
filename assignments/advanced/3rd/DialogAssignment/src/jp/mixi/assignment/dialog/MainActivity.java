6a1b54f83cbdb3267a4fb2cb478fae2c471b91bd
橋本 淳 <jun.hashimoto@mixi.co.jp>
2013-05-31 09:10:49 +0900

    solve DialogAssignment(temporary)

--- assignments/advanced/3rd/DialogAssignment/src/jp/mixi/assignment/dialog/MainActivity.java
+++ assignments/advanced/3rd/DialogAssignment/src/jp/mixi/assignment/dialog/MainActivity.java
@@ -1,12 +1,18 @@
 
 package jp.mixi.assignment.dialog;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -26,6 +32,11 @@ public class MainActivity extends FragmentActivity {
 
     private void showAssignmentDialog() {
         // TODO:ダイアログを表示する処理を実装してください
+    	DialogFragment myDialogFragment = new AssignmentDialogFragment();
+    	myDialogFragment.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+    	
+        // 引数にFramentManagerとtagを設定します
+        myDialogFragment.show(getSupportFragmentManager(), "my_dialog_fragment");
     }
 
     @Override
@@ -38,6 +49,30 @@ public class MainActivity extends FragmentActivity {
     // TODO:独自DialogFragmentを実装してください
     // TODO:コンテンツ領域にはEditTextを配置した独自レイアウトを使用してください。また、そのためのレイアウトxmlを作成してください。
     public static class AssignmentDialogFragment extends DialogFragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.dialog_content, container, false);
+            Button okButton = (Button) view.findViewById(R.id.okButton);
+            Button cancelButton = (Button) view.findViewById(R.id.cancelButton);
+            
+            okButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					Log.v("OK", "OK");
+				}
+			});
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+				}
+			});
+            return view;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+        	// Dialogを生成
+            Dialog dialog = super.onCreateDialog(savedInstanceState);
+            return dialog;
+        }
 
     }
 }
